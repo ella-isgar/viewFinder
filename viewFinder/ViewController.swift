a9d8e0b9fb1c3ad045ad0ae8b6e254b471e67f04
Ella Isgar <Ella@Apples-MacBook-Pro-63.local>
2019-08-13 09:59:01 -0400

    Completed Iteration 0

---
+++ viewFinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  viewFinder
+//
+//  Created by Ella Isgar on 8/13/19.
+//  Copyright © 2019 Ella Isgar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
