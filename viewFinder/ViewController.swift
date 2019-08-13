b2884cbab11d6bad85cbd0cddffda844770b2e2d
Ella Isgar <Ella@Apples-MacBook-Pro-63.local>
2019-08-13 10:20:07 -0400

    Completed Iteration 1a

--- viewFinder/ViewController.swift
+++ viewFinder/ViewController.swift
@@ -8,13 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    var imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        imagePicker.delegate = self
         // Do any additional setup after loading the view.
     }
-
-
 }
-
